572ea8feefa6abbb7436592d459cf94404ec8572
Tyler Leishman <tyler.leishman0@gmail.com>
2023-07-28 03:54:16 -0500

    add script objects

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "Great Cormorant",
+    link: "../images/great-cormorant.jpg",
+  },
+  {
+    name: "Kingfisher",
+    link: "../images/kingfisher.jpg",
+  },
+  {
+    name: "Green Parakeet",
+    link: "../images/green-parakeet.jpg",
+  },
+  {
+    name: "European Blue Jay",
+    link: "../images/eu-blue-jay.jpg",
+  },
+  {
+    name: "Black Crow",
+    link: "../images/crow.jpg",
+  },
+  {
+    name: "Canada Goose",
+    link: "../images/canada-goose.jpg",
+  },
+];
