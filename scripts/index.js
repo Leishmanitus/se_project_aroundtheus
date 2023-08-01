4bd193daed7cb64e130678bb855afdfd38876947
Tyler Leishman <tyler.leishman0@gmail.com>
2023-08-01 04:39:22 -0500

    add modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -24,7 +24,21 @@ let initialCards = [
     link: "../images/canada-goose.jpg",
   },
 ];
-function returnError(name) {
-  console.log("My name is ${name}");
+
+function toggleModal() {
+  const modal = document.querySelector(".modal");
+  if (modal.classList.contains("modal__opened")) {
+    modal.setAttribute("style", "display: none");
+    modal.classList.toggle("modal__opened");
+  } else {
+    modal.setAttribute("style", "display: flex");
+    modal.classList.toggle("modal__opened");
+  }
 }
-returnError();
+const editButton = document.querySelector(
+  ".profile .profile__button_type_edit"
+);
+editButton.addEventListener("click", toggleModal);
+
+const closeModalButton = document.querySelector(".modal .modal__close-button");
+closeModalButton.addEventListener("click", toggleModal);
