dc547eb85f3e9f4c3cb8c45af428c2933ef98214
Tyler Leishman <tyler.leishman0@gmail.com>
2023-08-03 04:29:22 -0500

    feat!:add user card initialization functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 //constant variables
 const modal = document.querySelector(".modal");
 const profile = document.querySelector(".profile");
+const cardTemplate = document.querySelector("#card-template").content;
 const editButton = profile.querySelector(".profile .profile__button_type_edit");
 const closeModalButton = modal.querySelector(".modal .modal__close-button");
 const submitFormButton = modal.querySelector(".form__save-button");
@@ -13,27 +14,33 @@ let profileDescription = profile.querySelector(".profile__description");
 let initialCards = [
   {
     name: "Great Cormorant",
-    link: "../images/great-cormorant.jpg",
+    link: "./images/great-cormorant.jpg",
+    alt: "A blue eyed Cormorant drying its wings",
   },
   {
     name: "Kingfisher",
-    link: "../images/kingfisher.jpg",
+    link: "./images/kingfisher.jpg",
+    alt: "A Kingfisher lurking in the water",
   },
   {
     name: "Green Parakeet",
-    link: "../images/green-parakeet.jpg",
+    link: "./images/green-parakeet.jpg",
+    alt: "A bright green parakeet in a tree knook",
   },
   {
     name: "European Blue Jay",
-    link: "../images/eu-blue-jay.jpg",
+    link: "./images/eu-blue-jay.jpg",
+    alt: "A brilliantly blue bird rests on a tree limb",
   },
   {
     name: "Black Crow",
-    link: "../images/crow.jpg",
+    link: "./images/crow.jpg",
+    alt: "A black crow cracking a nut open",
   },
   {
     name: "Canada Goose",
-    link: "../images/canada-goose.jpg",
+    link: "./images/canada-goose.jpg",
+    alt: "A Canada Goose with its duckling on its back",
   },
 ];
 
@@ -56,7 +63,21 @@ function handleProfileEditSubmission(event) {
   toggleModal();
 }
 
+function showCards(data) {
+  let cardContainer = document.querySelector(".page__cards");
+  for (let card of data) {
+    let userCard = cardTemplate.querySelector(".card").cloneNode(true);
+    userCard.querySelector(".card__image").src = card.link;
+    userCard.querySelector(".card__image").alt = card.alt;
+    userCard.querySelector(".card__caption").textContent = card.name;
+    cardContainer.append(userCard);
+  }
+}
+
 //button event listeners
 editButton.addEventListener("click", toggleModal);
 closeModalButton.addEventListener("click", toggleModal);
 submitFormButton.addEventListener("click", handleProfileEditSubmission);
+
+//initial calls
+showCards(initialCards);
