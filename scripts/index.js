ba3bef85683e5ecdb3fe5f0f876732ee01060aab
Tyler Leishman <tyler.leishman0@gmail.com>
2023-08-03 20:03:17 -0500

    fix: correct height on cards, optimize variable searches, create makeCard func

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,21 @@
 //constant variables
-const modal = document.querySelector(".modal");
 const profile = document.querySelector(".profile");
-const cardTemplate = document.querySelector("#card-template").content;
-const editButton = profile.querySelector(".profile .profile__button_type_edit");
-const closeModalButton = modal.querySelector(".modal .modal__close-button");
-const submitFormButton = modal.querySelector(".form__save-button");
+const profileName = profile.querySelector(".profile__name");
+const editButton = profile.querySelector(".profile__button_type_edit");
+const profileDescription = profile.querySelector(".profile__description");
 
-//let variables
-let modalName = modal.querySelector("#form-name");
-let modalDescription = modal.querySelector("#form-description");
-let profileName = profile.querySelector(".profile__name");
-let profileDescription = profile.querySelector(".profile__description");
-let initialCards = [
+const cardContainer = document.querySelector(".page__cards");
+const cardTemplate = cardContainer.querySelector("#card-template").content;
+const cardBody = cardTemplate.querySelector(".card");
+
+const modal = document.querySelector(".modal");
+const form = document.forms["profile-edit-form"];
+const formName = form.querySelector("#form-name");
+const formDescription = form.querySelector("#form-description");
+const submitForm = form.querySelector(".form__save-button");
+const closeModal = modal.querySelector(".modal__close-button");
+
+const initialCards = [
   {
     name: "Great Cormorant",
     link: "./images/great-cormorant.jpg",
@@ -46,13 +50,10 @@ let initialCards = [
 
 //functions
 function toggleModal() {
-  modal.classList.toggle("modal__opened");
-  if (modal.classList.contains("modal__opened")) {
-    modal.setAttribute("style", "display: flex");
-    modalName.value = profileName.textContent;
-    modalDescription.value = profileDescription.textContent;
-  } else {
-    modal.setAttribute("style", "display: none");
+  modal.classList.toggle("modal_opened");
+  if (modal.classList.contains("modal_opened")) {
+    formName.value = profileName.textContent;
+    formDescription.value = profileDescription.textContent;
   }
 }
 
@@ -63,21 +64,24 @@ function handleProfileEditSubmission(event) {
   toggleModal();
 }
 
+function makeCard(card) {
+  newCard = cardBody.cloneNode(true);
+  newCard.querySelector(".card__image").src = card.link;
+  newCard.querySelector(".card__image").alt = card.alt;
+  newCard.querySelector(".card__caption").textContent = card.name;
+  return newCard;
+}
+
 function showCards(data) {
-  let cardContainer = document.querySelector(".page__cards");
   for (let card of data) {
-    let userCard = cardTemplate.querySelector(".card").cloneNode(true);
-    userCard.querySelector(".card__image").src = card.link;
-    userCard.querySelector(".card__image").alt = card.alt;
-    userCard.querySelector(".card__caption").textContent = card.name;
-    cardContainer.append(userCard);
+    cardContainer.append(makeCard(card));
   }
 }
 
 //button event listeners
 editButton.addEventListener("click", toggleModal);
-closeModalButton.addEventListener("click", toggleModal);
-submitFormButton.addEventListener("click", handleProfileEditSubmission);
+submitForm.addEventListener("submit", handleProfileEditSubmission);
+closeModal.addEventListener("click", toggleModal);
 
 //initial calls
 showCards(initialCards);
